dec6ce33acbb2d45de5eef6e011ca5b5eba912cb
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-03-18 14:24:47 +0900

    add assets

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -4,42 +4,60 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour {
 
+	[SerializeField] private GameObject m_Muzzle;
+
 	private int m_BulletNum;
-	private int m_BulletLimit;
-	private int m_BulletBoxLimit;
-	private float m_CoolTime;
+	private int m_BulletLimit = 10;
+	private int m_BulletBoxLimit = 150;
+	private float m_CoolTime = 2.0f;
+	private float m_Interval = 2.0f;
 	private RaycastHit hit;
 	private Vector3 m_HitPoint;
-	private AudioClip audioClip;
-	private AudioSource audioSource;
+	private AudioClip m_AudioClip;
+	private AudioSource m_AudioSource;
 
 
 	// Use this for initialization
 	void Start () {
-		m_BulletNum =  30;
-		m_BulletLimit = 30;
-		m_BulletBoxLimit = 150;
-		m_CoolTime = 2.0f;
-		audioSource = gameObject.GetComponent<AudioSource>();
-		audioClip = audioSource.clip;
+		m_BulletNum = m_BulletLimit;
+		m_AudioSource = gameObject.GetComponent<AudioSource>();
+		m_AudioClip = m_AudioSource.clip;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-			audioSource.PlayOneShot (audioClip);
-			Shoot ();
+		if (m_Interval < 0.5f) {
+			m_Interval += Time.deltaTime;
+		} else {
+			if (Input.GetMouseButtonDown(0) && m_BulletNum > 0) {
+				m_AudioSource.PlayOneShot (m_AudioClip);
+				Shoot ();
+				m_Interval = 0;
+			}
 		}
 	}
 
 	void Shoot () {
 		Vector3 cameraCenter = new Vector3(Screen.width/2, Screen.height/2, 0);
 		Ray ray = Camera.main.ScreenPointToRay(cameraCenter);
+		m_BulletNum --;
 		if (Physics.Raycast(ray,out hit)){
-			m_HitPoint = hit.point;
-			GameObject fire = (GameObject)Resources.Load ("Prefabs/fire");
-			if (hit.collider.name != "kosuke") {
-				// Instantiate (fire, m_HitPoint, Quaternion.identity);
+			m_HitPoint = hit.point - ray.direction;
+			GameObject m_HitObjectSparkle = (GameObject)Resources.Load ("Prefabs/HitObjectSparkle");
+			GameObject m_MuzzleSparkle = (GameObject)Resources.Load ("Prefabs/MuzzleSparkle");
+			if (hit.collider) {
+				GameObject HitObjectSparkle = Instantiate (
+					m_HitObjectSparkle,
+					m_HitPoint,
+					Camera.main.transform.rotation
+				);
+				GameObject MuzzleSparkle = Instantiate (
+					m_MuzzleSparkle,
+					m_Muzzle.transform.position,
+					Camera.main.transform.rotation
+				);
+				Destroy(MuzzleSparkle, 0.1f);
+				Destroy(HitObjectSparkle, 0.5f);
 			}
 		}
 	}
