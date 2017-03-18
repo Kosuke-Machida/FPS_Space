55add41085b123cac2c0b80b9f0e8f189f9b34cd
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-03-18 10:22:00 +0900

    modified some places

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -30,7 +30,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private Camera m_Camera;
         private bool m_Jump;
-        private bool m_Squat;
         private float m_YRotation;
         private Vector2 m_Input;
         private Vector3 m_MoveDir = Vector3.zero;
