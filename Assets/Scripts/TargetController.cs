3fc61cc55808860db787cb034bbbaa12a976dddc
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-03-19 12:49:23 +0900

    add animation to target

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class TargetController : MonoBehaviour {
 
-	private int m_Life = 5;
+	[SerializeField] GameObject m_Target;
+
+	private Animator anim;
+	private int m_Life;
+	private int m_MaxLife = 5;
+	private float m_PassedTimeAfterDead;
 
 	// Use this for initialization
 	void Start () {
-		
+		m_Life = m_MaxLife;
+		m_PassedTimeAfterDead = 0;
+		anim = m_Target.GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		if (m_Life == 0) {
+			anim.SetBool("IsDead", true);
+			m_PassedTimeAfterDead += Time.deltaTime;
+			if (m_PassedTimeAfterDead > 5.0f){
+				anim.SetBool("IsDead", false);
+				m_Life = 5;
+				m_PassedTimeAfterDead = 0;
+			}
+		}
 	}
 
 	public void Hit () {
