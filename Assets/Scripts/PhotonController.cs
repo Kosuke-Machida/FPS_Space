cc444089f62916338978d5d420f34da5b981b3ab
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-04-01 14:17:41 +0900

    modified some bugs

--- Assets/Scripts/PhotonController.cs
+++ Assets/Scripts/PhotonController.cs
@@ -18,11 +18,17 @@ public class PhotonController : MonoBehaviour {
     }
 
     void OnJoinedRoom(){
-        PhotonNetwork.Instantiate(
-            _player.name,
-            new Vector3(0f, 0f, 0f),
-            Quaternion.identity,
-            0
-         );
+        GameObject _myPlayer = PhotonNetwork.Instantiate(
+                                    _player.name,
+                                    new Vector3(0f, 0f, 0f),
+                                    Quaternion.identity,
+                                    0
+                               );
+
+        _myPlayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+        _myPlayer.GetComponent<AudioListener>().enabled = true;
+
+        GameObject _mainCamera = GameObject.FindWithTag("MainCamera");
+        _mainCamera.GetComponent<Camera>().enabled = true;
     }
 }
